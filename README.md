e33d23d60f59fb0acfcfdbb47c66a80a321b6ddf
David Canto <david@davidcphoto.com>
2025-05-20 00:19:23 +0100

    Versão inicial

---
+++ README.md
@@ -0,0 +1,38 @@
+# zuppercase README
+
+Automatically converts to uppercase any key pressed for the mainframe languages cobol, jcl, rexx and high level assembler.
+
+## Features
+
+- Activate and deactivate zUpperCase for individual mainframe languages in workspace settings.
+
+- Activate and deactivate zUpperCase for active text document language in status bar.
+
+    ![StatusBar.png](imagens\\StatusBar.png)
+
+## Requirements
+
+n/a
+
+## Extension Settings
+
+
+This extension contributes the following settings:
+
+* `zUpperCase.UpperCaseCobolDocumentos`: When active writing ***cobol*** documents automatically will be converted to upper case.
+* `zUpperCase.UpperCaseJclDocumentos`: When active writing ***jcl*** documents automatically will be converted to upper case.
+* `zUpperCase.UpperCaseRexxDocumentos`: When active writing ***rexx*** documents automatically will be converted to upper case.
+* `zUpperCase.UpperCaseHlasmDocumentos`: When active writing ***HighHigh Level Assembler*** documents automatically will be converted to upper case.
+
+## Known Issues
+
+n/a
+
+## Release Notes
+
+Users appreciate release notes as you update your extension.
+
+### 1.0.0
+
+Initial release of zUpperCase
+
---
+++ extension.js
@@ -0,0 +1,153 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+let Alteração = true;
+let Utexto;
+
+let myStatusBarItem;
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	vscode.window.onDidChangeActiveTextEditor(() => {
+		myStatusBarItem.hide();
+		Statusbar();
+	});
+
+
+	vscode.workspace.onDidChangeConfiguration(() => {
+		Statusbar();
+	});
+
+	vscode.workspace.onDidChangeTextDocument(Alterações => {
+
+		Statusbar();
+
+		Utexto = Alterações.contentChanges[0];
+		const validaCobol = vscode.workspace.getConfiguration('zUpperCase').get('UpperCaseCobolDocumentos');
+		const validaJcl = vscode.workspace.getConfiguration('zUpperCase').get('UpperCaseJclDocumentos');
+		const validaRexx = vscode.workspace.getConfiguration('zUpperCase').get('UpperCaseRexxDocumentos');
+		const validaHlasm = vscode.workspace.getConfiguration('zUpperCase').get('UpperCaseHlasmDocumentos');
+
+
+		if ((Alterações.document.languageId == "cobol" && validaCobol) ||
+			(Alterações.document.languageId == "jcl" && validaJcl) ||
+			(Alterações.document.languageId == "rexx" && validaRexx) ||
+			(Alterações.document.languageId == "hlasm" && validaHlasm)) {
+
+
+
+
+			if (Alteração && Utexto.text != Utexto.text.toUpperCase()) {
+
+				vscode.window.activeTextEditor.edit(edit => {
+
+
+					let linhaInicio =0;
+					let linhaFim =0;
+					let inicio = 0;
+					let fim = 0;
+
+					linhaInicio = Utexto.range.start.line;
+					inicio = Utexto.range.start.character;
+
+					if (Utexto.rangeLength.length == 1) {
+						linhaFim = Utexto.range.end.line;
+						fim = Alterações.contentChanges[0].range.end.character + 1;
+					} else {
+						linhaFim = Utexto.range.start.line;
+						fim = Alterações.contentChanges[0].range.start.character + 1;
+					}
+
+
+					const Inicio = new vscode.Position(linhaInicio, inicio);
+					const Fim = new vscode.Position(linhaFim, fim);
+
+
+
+					const Utextotext = String(Utexto.text).toUpperCase();
+					const UtextoRange = new vscode.Range(Inicio, Fim);
+					edit.replace(UtextoRange, Utextotext);
+					Alteração = false;
+				})
+
+
+			} else {
+				Alteração = true;
+			}
+		}
+
+	})
+
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "zuppercase" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('zuppercase.Mudar', function () {
+		// The code you place here will be executed every time your command is executed
+
+		const Linguagem = vscode.window.activeTextEditor.document.languageId;
+		const linguagemCapitalized = Linguagem.charAt(0).toUpperCase() + Linguagem.slice(1);
+		let LinguagemCase = true;
+		LinguagemCase = vscode.workspace.getConfiguration('zUpperCase').get(`UpperCase${linguagemCapitalized}Documentos`);
+		vscode.workspace.getConfiguration('zUpperCase').update(`UpperCase${linguagemCapitalized}Documentos`, !LinguagemCase).then(() => {
+
+			Statusbar();
+
+		});
+
+
+	});
+
+
+	// create a new status bar item that we can now manage
+	myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 1);
+	Statusbar();
+
+	// statusBarItem.command = 'zuppercase.Mudar';
+
+	context.subscriptions.push(disposable);
+	context.subscriptions.push(myStatusBarItem);
+}
+
+// This method is called when your extension is deactivated
+function deactivate() { }
+
+module.exports = {
+	activate,
+	deactivate
+}
+
+function Statusbar() {
+
+	const Linguagem = vscode.window.activeTextEditor.document.languageId;
+	if (["cobol", "rexx", "jcl", "hlasm"].includes(Linguagem)) {
+		const linguagemCapitalized = Linguagem.charAt(0).toUpperCase() + Linguagem.slice(1);
+		let LinguagemCase = true;
+		LinguagemCase = vscode.workspace.getConfiguration('zUpperCase').get(`UpperCase${linguagemCapitalized}Documentos`);
+		let icon = '';
+
+		if (LinguagemCase) {
+			icon = "$(pass)";
+		} else {
+			icon = "$(stop)";
+		}
+
+		myStatusBarItem.command = 'zuppercase.Mudar';
+		myStatusBarItem.name = 'zUpperCase';
+		myStatusBarItem.text = `zUpperCase ${icon}`;
+		myStatusBarItem.tooltip = `zUpperCase - Uppercase for the language ${vscode.window.activeTextEditor.document.languageId} is ${LinguagemCase}`;
+		myStatusBarItem.show();
+	} else {
+		myStatusBarItem.hide();
+	}
+}
